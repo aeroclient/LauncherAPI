e3788b675d996d410f9432250a76778c812f8356
ArcaneCiCi <59527329+ArcaneCiCi@users.noreply.github.com>
2020-10-09 21:55:02 -0400

    Update banned.js

--- banned.js
+++ banned.js
@@ -1,7 +1,14 @@
 function bannedChecker() {
   fs.readFile(getAppDataPath(".minecraft/launcher_profiles.json"), function (err, data) {
-  if (err) throw err;if(data.indexOf('PLAYER-NAME-1') >= 1){banUser()}});
-  
+    if (err) throw err;
+    if(data.indexOf('AntiGamingPhone') >= 1){
+     banUser()
+    }
+  });
   fs.readFile(getAppDataPath(".minecraft/launcher_profiles.json"), function (err, data) {
-  if (err) throw err;if(data.indexOf('PLAYER-NAME-2') >= 1){banUser()}});
+    if (err) throw err;
+    if(data.indexOf('rbuh') >= 1){
+     banUser()
+    }
+  });
 }
