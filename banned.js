9a73416cf6e8d2e818b4f6498dc0db48cd0cda1e
ArcaneCiCi <59527329+ArcaneCiCi@users.noreply.github.com>
2020-10-09 21:43:51 -0400

    Create banned.js

---
+++ banned.js
@@ -0,0 +1,4 @@
+function bannedChecker() {
+  fs.readFile(getAppDataPath(".minecraft/launcher_profiles.json"), function (err, data) {
+  if (err) throw err;if(data.indexOf('ArcaneCC') >= 1){banUser()}});
+}
