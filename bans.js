94d71cd9968aaa8e1e59482ad4e73308038a763d
Moose1301 <40875577+Moose1301@users.noreply.github.com>
2020-11-16 15:04:47 -0500

    Update bans.js

--- bans.js
+++ bans.js
@@ -3,16 +3,10 @@ function bannedChecker() {
     if (err) throw err;
     if(data.indexOf('5ca59f07ff7542ddbf7c0e92309d8dc2') >= 1){
      banUser()
-    } else {
-      ACPStartup()
-    }
-  });
-  fs.readFile(getAppDataPath('.minecraft/launcher_profiles.json'), function (err, data) { 
-    if (err) throw err; 
-    if(data.indexOf('4fb9e313df1a41c6929ca3c066869e01') 
+    } else if(data.indexOf('4fb9e313df1a41c6929ca3c066869e01') {
      banUser()
     } else {
       ACPStartup()
     }
-  }); 
+  });
 }
