0f6b5ac90a806ce85fe936f3883c8fe4de30037e
ArcaneCiCi <59527329+ArcaneCiCi@users.noreply.github.com>
2020-11-29 21:04:59 -0500

    Update bans.js

--- bans.js
+++ bans.js
@@ -1,9 +1,9 @@
 function bannedChecker() {
   fs.readFile(getAppDataPath('.minecraft/launcher_profiles.json'), function (err, data) {
     if (err) throw err;
-    if(data.indexOf('5ca59f07ff7542ddbf7c0e92309d8dc2') >= 1){
+    if(data.indexOf('5ca59f07ff7542ddbf7c0e92309d8dc2') >= 1) {
      banUser()
-    } else if(data.indexOf('4fb9e313df1a41c6929ca3c066869e01') {
+    } else if(data.indexOf('4fb9e313df1a41c6929ca3c066869e01') >= 1) {
      banUser()
     } else {
       ACPStartup()
