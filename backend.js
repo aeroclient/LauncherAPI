d42c10f472f466890ec52a88e2b8b0f7275c22ad
ArcaneCiCi <59527329+ArcaneCiCi@users.noreply.github.com>
2020-11-29 21:41:42 -0500

    Create backend.js

---
+++ backend.js
@@ -0,0 +1,10 @@
+function banUser() {
+  document.querySelector('.launch-button').className += " banned";
+  document.getElementById('launchButton').innerHTML = "BANNED";
+  console.log("[ACP]: ACP_LAUNCH_BANNED");
+}
+
+function startupChecks() {
+  bannedChecker()
+}
+
