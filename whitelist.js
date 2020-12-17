3ccb8971bf30d1b627283421c3bf14f092d00e95
ArcaneCiCi <59527329+ArcaneCiCi@users.noreply.github.com>
2020-12-16 22:20:22 -0500

    Update whitelist.js

--- whitelist.js
+++ whitelist.js
@@ -1,5 +1,4 @@
 function whitelistCheck() {
-  document.getElementById('launchButton').innerHTML = "Checking Whitelist";
   fs.readFile(getAppDataPath('.minecraft/launcher_accounts.json'), function (err, data) {
     if (err) throw err;
     if(data.indexOf('5802512617c24fe3bbc981519341f7d6') >= 1) {
