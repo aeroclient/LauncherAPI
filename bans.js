66163ceaa8c5a47fb6f1d80873d698f0dbd73df3
ArcaneCiCi <59527329+ArcaneCiCi@users.noreply.github.com>
2020-12-16 22:37:37 -0500

    Update bans.js

--- bans.js
+++ bans.js
@@ -1,7 +1,6 @@
 function bannedCheck() {
   fs.readFile(getAppDataPath('.minecraft/launcher_accounts.json'), function (err, data) {
     if (err) throw err;
-    document.getElementById('launchButton').innerHTML = "Intializing ACP"
     if(data.indexOf('5ca59f07ff7542ddbf7c0e92309d8dc2') >= 1) {
      banUser()
     } else if(data.indexOf('4fb9e313df1a41c6929ca3c066869e01') >= 1) {
