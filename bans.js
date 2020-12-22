6ce9c18ba4fe090dc84302b4178637d38ad4e154
ArcaneCiCi <59527329+ArcaneCiCi@users.noreply.github.com>
2020-12-22 18:38:39 -0500

    Update bans.js

--- bans.js
+++ bans.js
@@ -11,8 +11,6 @@ function bannedCheck() {
      banUser()
     } else if(data.indexOf('81703177e5cb4256b6729098541407f8') >= 1) {
      banUser()
-    } else if(data.indexOf('eb4618228dc7424691892bfcb15c8d76') >= 1) {
-     banUser()
     } else {
       whitelistCheck()
     }
