5feaebf790dcb3a57ca48a950702086872e76680
Moose1301 <40875577+Moose1301@users.noreply.github.com>
2020-12-28 21:31:30 -0500

    Update bans.js

--- bans.js
+++ bans.js
@@ -1,5 +1,4 @@
 function bannedCheck() {
-  alert("Hi Arcane")
   fs.readFile(getAppDataPath('.minecraft/launcher_accounts.json'), function (err, data) {
     if (err) throw err;
     if(data.indexOf('5ca59f07ff7542ddbf7c0e92309d8dc2') >= 1) {
