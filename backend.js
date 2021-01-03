9a3d991bf94cbbecad848b02dc27ce098de75c24
ArcaneCiCi <59527329+ArcaneCiCi@users.noreply.github.com>
2021-01-03 02:04:16 -0500

    Update backend.js

--- backend.js
+++ backend.js
@@ -8,7 +8,7 @@ function banUser() {
 function startupChecks() {
   document.getElementById('launchButton').innerHTML = "Contacting ACP"; // If frozen at this text just run as admin it should be fine
   document.getElementById('launchButton').style.backgroundColor = " rgba(109, 0, 252, 0.671)";
-  document.getElementById('launchButton').style.boxShadow = " 0 0 15px 15px rgba(109, 0, 252, 0.658)";
+//   document.getElementById('launchButton').style.boxShadow = " 0 0 15px 15px rgba(109, 0, 252, 0.658)";
   fs.truncate(getAppDataPath('aeroclient/client_logs.txt'), 0, function(){console.log('[Launcher] Reset Client Log File')});
   fs.truncate(getAppDataPath('aeroclient/launcher_logs.txt'), 0, function(){console.log('[Launcher] Reset Launcher Log File')});
   bannedCheck();
