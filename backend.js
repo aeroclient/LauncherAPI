13bed60aa85ebeb5682dd6aaa751677260a55267
ArcaneCiCi <59527329+ArcaneCiCi@users.noreply.github.com>
2021-01-04 00:35:48 -0500

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,6 +1,7 @@
 function banUser() {
   document.getElementById('launchButton').style.backgroundColor = " rgba(252, 17, 0, 0.753)";
   document.getElementById('launchButton').innerHTML = "Banned";
+  document.querySelector('.launch-button').className += " banned";
   alert("You are banned from Aero Client. Appeal at https://aeroclient.net/appeal");
   console.log("[ACP]: ACP_LAUNCH_BANNED");
 }
