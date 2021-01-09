f2310e0c60e40464517b0063f8e381b5381e56e0
ArcaneCiCi <59527329+ArcaneCiCi@users.noreply.github.com>
2021-01-08 19:47:49 -0500

    Update backend.js

--- backend.js
+++ backend.js
@@ -2,7 +2,6 @@ function banUser() {
   document.getElementById('launchButton').style.backgroundColor = " rgba(252, 17, 0, 0.753)";
   document.getElementById('launchButton').innerHTML = "Banned";
   document.querySelector('.launch-button').className += " banned";
-  alert("You are banned from Aero Client. Appeal at https://aeroclient.net/appeal");
   console.log("[ACP]: ACP_LAUNCH_BANNED");
 }
 
@@ -17,7 +16,6 @@ function startupChecks() {
 function notWhitelisted() {
   document.getElementById('launchButton').style.backgroundColor = " rgba(252, 17, 0, 0.753)";
   document.getElementById('launchButton').innerHTML = "Unauthorized";
-  alert("You are not authorized to launch Aero Client at this time.");
   console.log("[ACP]: Not whitelisted!");
 }
 
