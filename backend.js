f68edd2ddafb3108b3bb707b1aec6318f5120d7b
ArcaneCiCi <59527329+ArcaneCiCi@users.noreply.github.com>
2021-01-25 11:36:41 -0500

    Update backend.js

--- backend.js
+++ backend.js
@@ -21,6 +21,17 @@ function notWhitelisted() {
 }
 
 function checkPatch() { // This Method fixes the client from downloading itself everytime
+  var version2 = document.querySelector('.version-button2').innerHTML
+  var version3 = document.querySelector('.version-button3').innerHTML
+
+   if (version2 == "✔️1.8.9") {
+    alert("Aero Client 1.8.9 is not released, we will announce when it is though!");
+     showLauncher();
+  } else if(version3 == "✔️1.16") {
+    alert("Aero Client 1.16 is not released, we will announce when it is though!");
+    showLauncher();
+  }
+  
   if (!(fs.existsSync(getAppDataPath('.minecraft/versions/AeroClient-1.7.10/AeroClient-1.7.10.patch')))) { // If the client doesn't exist it will download the client
     launchClient1();
   }
