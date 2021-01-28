8ceea6d38acfda44b06e33998dfafe79bd2c57b9
ArcaneCiCi <59527329+ArcaneCiCi@users.noreply.github.com>
2021-01-28 13:51:55 -0500

    Update backend.js

--- backend.js
+++ backend.js
@@ -38,7 +38,7 @@ function checkPatch() { // This Method fixes the client from downloading itself 
   
     var hash = clientPatch.sync(getAppDataPath('.minecraft/versions/AeroClient-1.7.10/AeroClient-1.7.10.patch'));
     console.log(hash)
-    if (hash == "c7a0a739702ea8576e74f8e2f60e627c94387dee") { // Checks the SHA1 Hash to see if it's outdated or not
+    if (hash == "311178adf3006fd13d6c562d553629a709c80618") { // Checks the SHA1 Hash to see if it's outdated or not
       launchOffline1();
     } else {
       launchClient1(); // If the SHA1 Hash is different then it will download the client
