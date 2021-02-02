b4cf99ce4e57df30131a147088ad85b1f10c11ea
ArcaneCiCi <59527329+ArcaneCiCi@users.noreply.github.com>
2021-02-01 19:33:20 -0500

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,64 +1,64 @@
 function banUser() {
-  document.getElementById('launchButton').style.backgroundColor = " rgba(252, 17, 0, 0.753)";
-  document.getElementById('launchButton').innerHTML = "Banned";
-  document.querySelector('.launch-button').className += " banned";
-  console.log("[ACP]: ACP_LAUNCH_BANNED");
-}
-
-function startupChecks() {
-  document.getElementById('launchButton').innerHTML = "Contacting ACP"; // If frozen at this text just run as admin it should be fine
-  document.getElementById('launchButton').style.backgroundColor = " rgba(109, 0, 252, 0.671)";
-  fs.truncate(getAppDataPath('aeroclient/client_logs.txt'), 0, function(){console.log('[Launcher] Reset Client Log File')});
-  fs.truncate(getAppDataPath('aeroclient/launcher_logs.txt'), 0, function(){console.log('[Launcher] Reset Launcher Log File')});
-  bannedCheck();
-}
-
-function notWhitelisted() {
-        document.getElementById('launchButton').style.backgroundColor = " rgba(252, 17, 0, 0.753)";
-        document.getElementById('launchButton').innerHTML = "Unauthorized";
-        document.querySelector('.launch-button').className += " banned";
-        console.log("[ACP]: Not whitelisted!");
-}
-
-function checkPatchMain() {
- bannedCheck();
-}
-
-function checkPatch() { // This Method fixes the client from downloading itself everytime
-  var version2 = document.querySelector('.version-button2').innerHTML
-  var version3 = document.querySelector('.version-button3').innerHTML
-
-   if (version2 == "✔️1.8.9") {
-    alert("Aero Client 1.8.9 is not released, we will announce when it is though!");
-     showLauncher();
-  } else if(version3 == "✔️1.16") {
-    alert("Aero Client 1.16 is not released, we will announce when it is though!");
-    showLauncher();
-  } else {
-  
-  if (!(fs.existsSync(getAppDataPath('.minecraft/versions/AeroClient-1.7.10/AeroClient-1.7.10.patch')))) { // If the client doesn't exist it will download the client
-    launchClient1();
+    document.getElementById('launchButton').style.backgroundColor = " rgba(252, 17, 0, 0.753)";
+    document.getElementById('launchButton').innerHTML = "Banned";
+    document.querySelector('.launch-button').className += " banned";
+    console.log("[ACP]: ACP_LAUNCH_BANNED");
   }
   
-    var hash = clientPatch.sync(getAppDataPath('.minecraft/versions/AeroClient-1.7.10/AeroClient-1.7.10.patch'));
-    console.log(hash)
-    if (hash == "ee413f0d4fb8d69a8833a3ee76974bad77f90c2c") { // Checks the SHA1 Hash to see if it's outdated or not
-      launchOffline1();
+  function startupChecks() {
+    document.getElementById('launchButton').innerHTML = "Contacting ACP"; // If frozen at this text just run as admin it should be fine
+    document.getElementById('launchButton').style.backgroundColor = " rgba(109, 0, 252, 0.671)";
+    fs.truncate(getAppDataPath('aeroclient/client_logs.txt'), 0, function(){console.log('[Launcher] Reset Client Log File')});
+    fs.truncate(getAppDataPath('aeroclient/launcher_logs.txt'), 0, function(){console.log('[Launcher] Reset Launcher Log File')});
+    bannedCheck();
+  }
+  
+  function notWhitelisted() {
+          document.getElementById('launchButton').style.backgroundColor = " rgba(252, 17, 0, 0.753)";
+          document.getElementById('launchButton').innerHTML = "Unauthorized";
+          document.querySelector('.launch-button').className += " banned";
+          console.log("[ACP]: Not whitelisted!");
+  }
+  
+  function startupChecksMain() {
+    checkPatch();
+  }
+
+  function checkPatch() { // This Method fixes the client from downloading itself everytime
+    var version2 = document.querySelector('.version-button2').innerHTML
+    var version3 = document.querySelector('.version-button3').innerHTML
+  
+     if (version2 == "✔️1.8.9") {
+      alert("Aero Client 1.8.9 is not released, we will announce when it is though!");
+       showLauncher();
+    } else if(version3 == "✔️1.16") {
+      alert("Aero Client 1.16 is not released, we will announce when it is though!");
+      showLauncher();
     } else {
-      launchClient1(); // If the SHA1 Hash is different then it will download the client
-        }
+    
+    if (!(fs.existsSync(getAppDataPath('.minecraft/versions/AeroClient-1.7.10/AeroClient-1.7.10.patch')))) { // If the client doesn't exist it will download the client
+      launchClient1();
     }
-}
-
-function checkBranch2() {
-  var branch2 = document.querySelector('.branch-button2').innerHTML
-  var branch3 = document.querySelector('.branch-button3').innerHTML
-
- if(branch2 == "✔️Beta") {
-    console.log("[Aero Client] Checked Beta Branch");
-    launchClient2();
-  } else if(branch3 == "✔️Dev") {
-    console.log("[Aero Client] Checked Dev Branch");
-    launchClient3();
+    
+      var hash = clientPatch.sync(getAppDataPath('.minecraft/versions/AeroClient-1.7.10/AeroClient-1.7.10.patch'));
+      console.log(hash)
+      if (hash == "ee413f0d4fb8d69a8833a3ee76974bad77f90c2c") { // Checks the SHA1 Hash to see if it's outdated or not
+        launchOffline1();
+      } else {
+        launchClient1(); // If the SHA1 Hash is different then it will download the client
+          }
+      }
   }
-}
+  
+  function checkBranch2() {
+    var branch2 = document.querySelector('.branch-button2').innerHTML
+    var branch3 = document.querySelector('.branch-button3').innerHTML
+  
+   if(branch2 == "✔️Beta") {
+      console.log("[Aero Client] Checked Beta Branch");
+      launchClient2();
+    } else if(branch3 == "✔️Dev") {
+      console.log("[Aero Client] Checked Dev Branch");
+      launchClient3();
+    }
+  }
