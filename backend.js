926df505cc285b894f2700d7305150110563eafd
ArcaneCiCi <59527329+ArcaneCiCi@users.noreply.github.com>
2021-02-06 00:05:58 -0500

    Update backend.js

--- backend.js
+++ backend.js
@@ -42,7 +42,7 @@ function banUser() {
     
       var hash = clientPatch.sync(getAppDataPath('.minecraft/versions/AeroClient-1.7.10/AeroClient-1.7.10.patch'));
       console.log(hash)
-      if (hash == "ce9181f7d0387b3dc9e3d5ed03e59910073ad740") { // Checks the SHA1 Hash to see if it's outdated or not
+      if (hash == "4c7348f53b607624c458d308c0118bd2097b68a9") { // Checks the SHA1 Hash to see if it's outdated or not
         launchOffline1();
       } else {
         launchClient1(); // If the SHA1 Hash is different then it will download the client
