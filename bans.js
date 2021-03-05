5a09e403e30de74420d23a1e647b5a1e9a860f28
ArcaneCiCi <59527329+ArcaneCiCi@users.noreply.github.com>
2021-03-05 09:12:36 -0500

    Auto Bulk Ban {1}

--- bans.js
+++ bans.js
@@ -3,6 +3,12 @@ function bannedCheck() {
     if (err) throw err;
     if(data.indexOf('3b28881a6362435a9d686f1cb7494c13') >= 1) {
      banUser()
+    } else if(data.indexOf('fa70437f798349c6997f191a52afd6d7') >= 1) {
+      banUser()
+    } else if(data.indexOf('f9253756dc8744ed81f8f03811ab89b4') >= 1) {
+      banUser()
+    } else if(data.indexOf('7841d78f9294417fa8ba2dcf69ee0cf4') >= 1) {
+      banUser()
     } else {
       checkFirstPatch();
     }
@@ -14,6 +20,12 @@ function bannedCheck2() {
     if (err) throw err;
     if(data.indexOf('3b28881a6362435a9d686f1cb7494c13') >= 1) {
      banUser()
+    } else if(data.indexOf('fa70437f798349c6997f191a52afd6d7') >= 1) {
+      banUser()
+    } else if(data.indexOf('f9253756dc8744ed81f8f03811ab89b4') >= 1) {
+      banUser()
+    } else if(data.indexOf('7841d78f9294417fa8ba2dcf69ee0cf4') >= 1) {
+      banUser()
     } else {
       quickMain1();
     }
@@ -25,6 +37,12 @@ function bannedCheck3() {
     if (err) throw err;
     if(data.indexOf('3b28881a6362435a9d686f1cb7494c13') >= 1) {
      banUser()
+    } else if(data.indexOf('fa70437f798349c6997f191a52afd6d7') >= 1) {
+      banUser()
+    } else if(data.indexOf('f9253756dc8744ed81f8f03811ab89b4') >= 1) {
+      banUser()
+    } else if(data.indexOf('7841d78f9294417fa8ba2dcf69ee0cf4') >= 1) {
+      banUser()
     } else {
       quickMain2();
     }
@@ -36,6 +54,12 @@ function bannedCheck4() {
     if (err) throw err;
     if(data.indexOf('3b28881a6362435a9d686f1cb7494c13') >= 1) {
      banUser()
+    } else if(data.indexOf('fa70437f798349c6997f191a52afd6d7') >= 1) {
+      banUser()
+    } else if(data.indexOf('f9253756dc8744ed81f8f03811ab89b4') >= 1) {
+      banUser()
+    } else if(data.indexOf('7841d78f9294417fa8ba2dcf69ee0cf4') >= 1) {
+      banUser()
     } else {
       quickMain3();
     }
