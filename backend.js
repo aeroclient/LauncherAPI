122c56ae7dc1e45ec4eb4b4a9a757c8c7e91da6b
ArcaneCiCi <59527329+ArcaneCiCi@users.noreply.github.com>
2021-03-15 14:49:10 -0400

    Update backend.js

--- backend.js
+++ backend.js
@@ -23,19 +23,7 @@ function banUser() {
   }
   
 function startupChecksMain() {
-  var version1 = document.querySelector('.version-button1').innerHTML
-  var version2 = document.querySelector('.version-button2').innerHTML
-  var version3 = document.querySelector('.version-button3').innerHTML
-
-  if (version2 == "✔️1.8.9") {
-    alert("Aero Client 1.8.9 is not released, we will announce when it is though!");
-    showLauncher();
-  } else if(version3 == "✔️1.16") {
-    alert("Aero Client 1.16 is not released, we will announce when it is though!");
-    showLauncher();
-  } else if (version1 == "✔️1.7.10 (Default)") {
     checkFirstPatch();
-  }
 }
 
 function checkPatch() { // This Method fixes the client from downloading itself everytime.
