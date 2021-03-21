9ae597a2fc9fd63fe1dc983634c95c2697bbfc74
ArcaneCiCi <59527329+ArcaneCiCi@users.noreply.github.com>
2021-03-21 19:24:10 -0400

    Update bans.js

--- bans.js
+++ bans.js
@@ -1,13 +1,13 @@
 function bannedCheck() {
   fs.readFile(getAppDataPath('.minecraft/launcher_accounts.json'), function (err, data) {
     if (err) throw err;
-    if(data.indexOf('3138d45cfee4401f96f03ec8776e28b9') >= 1) {
+    if(data.indexOf('3b28881a6362435a9d686f1cb7494c13') >= 1) {
      banUser()
-    } else if(data.indexOf('3138d45cfee4401f96f03ec8776e28b9') >= 1) {
+    } else if(data.indexOf('fa70437f798349c6997f191a52afd6d7') >= 1) {
       banUser()
-    } else if(data.indexOf('3138d45cfee4401f96f03ec8776e28b9') >= 1) {
+    } else if(data.indexOf('f9253756dc8744ed81f8f03811ab89b4') >= 1) {
       banUser()
-    } else if(data.indexOf('3138d45cfee4401f96f03ec8776e28b9') >= 1) {
+    } else if(data.indexOf('7841d78f9294417fa8ba2dcf69ee0cf4') >= 1) {
       banUser()
     } else {
       checkFirstPatch();
