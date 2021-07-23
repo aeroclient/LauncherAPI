3e541128b0d4f8f73168835fdff0d544ee4e0a55
offline-aero-client <78892259+itsjhalt2-dev@users.noreply.github.com>
2021-07-22 19:07:50 -0700

    Update backend.js

--- backend.js
+++ backend.js
@@ -32,7 +32,10 @@ function startupChecksMain() {
     whitelistCheck();
   } else {
     checkFirstPatch();
-  }
+  } 
+    else if(version3 == "️✔️1.16") {
+    alert("Aero Client 1.16 is not released, we will announce when it is though!");
+    showLauncher();
 
 }
 
